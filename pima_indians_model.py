a02eaf866591e931fdb5dc48d2519b92ef99c9dd
darangov <darangov@gmail.com>
2018-08-08 22:35:14 -0500

    update .py file

--- pima_indians_model.py
+++ pima_indians_model.py
@@ -29,7 +29,7 @@ X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_s
 model = LogisticRegression()
 model.fit(X_train, Y_train)
 # save the model to disk
-filename = 'finalized_model.sav'
+filename = 'finalized_model_pickle.sav'
 pickle.dump(model, open(filename, 'wb'))
 
 # some time later...
@@ -38,3 +38,44 @@ pickle.dump(model, open(filename, 'wb'))
 loaded_model = pickle.load(open(filename, 'rb'))
 result = loaded_model.score(X_test, Y_test)
 print(result)
+
+# ---------------- FIN PICKLE ----------------------------------------------------
+# --------------------------------------------------------------------------------
+
+# ----------- Save Model Using joblib --------------------------------------------
+import pandas
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+from sklearn.externals import joblib
+"""
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pandas.read_csv(url, names=names)
+"""
+#Data desde archivo local
+filename = 'pima-indians-diabetes.data.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pandas.read_csv(filename, names=names)
+#dataset = pandas.read_csv(filename, names=names)
+
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+test_size = 0.33
+seed = 7
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=test_size, random_state=seed)
+# Fit the model on 33%
+model1 = LogisticRegression()
+model1.fit(X_train, Y_train)
+# save the model to disk
+filename = 'finalized_model_joblib.sav'
+joblib.dump(model1, filename)
+
+# some time later...
+
+# load the model from disk
+loaded_model = joblib.load(filename)
+result = loaded_model.score(X_test, Y_test)
+print(result)
+
+# -------------------FIN JOBLIB --------------------------------------------
